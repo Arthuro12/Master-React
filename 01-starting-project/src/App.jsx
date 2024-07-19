ddc15068b858c3d43e137181a21e3a5e36cd32aa
Arthuro12 <arthurfoadjo@yahoo.com>
2024-07-19 08:45:59 +0200

    The special children prop

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,5 +1,6 @@
 import CoreConcept from "./components/CoreConcept.jsx";
 import Header from "./components/Header/Header.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 import reactImg from "./assets/react-core-concepts.png";
 import { CORE_CONCEPTS } from "./data.js";
@@ -36,6 +37,12 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Component</TabButton>
+          </menu>
         </section>
       </main>
     </div>
