fe0a60f555c4c7708e1c1b543782d0cb30092778
Arthuro12 <arthurfoadjo@yahoo.com>
2024-07-19 08:27:21 +0200

    Move header css styles to its own file

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,5 +1,5 @@
 import CoreConcept from "./components/CoreConcept.jsx";
-import Header from "./components/Header.jsx";
+import Header from "./components/Header/Header.jsx";
 
 import reactImg from "./assets/react-core-concepts.png";
 import { CORE_CONCEPTS } from "./data.js";
