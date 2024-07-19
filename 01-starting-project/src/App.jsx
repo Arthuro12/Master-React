1d9f4a4dde5ec206c79f8055742fee8dcead10d8
Arthuro12 <arthurfoadjo@yahoo.com>
2024-07-19 14:24:09 +0200

    Conditionally rendering with if-statement

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -29,9 +29,23 @@ function App() {
   // }
   // let content = "Please click a button";
 
+  let tabContent = <p>Please select a topic.</p>;
+
   const handleSelect = (selectedButton) => {
     setSelectedTopic(selectedButton);
   };
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -59,16 +73,7 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {!selectedTopic && <p>Please select a topic.</p>}
-          {selectedTopic && (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          )}
+          {tabContent}
         </section>
       </main>
     </div>
