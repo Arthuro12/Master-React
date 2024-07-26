3330f19ddd8d67a385ccbcc9efa3faee721683ee
Arthuro12 <arthurfoadjo@yahoo.com>
2024-07-26 02:47:28 +0200

    Add Player.jsx component for the layout of player section

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,16 +1,12 @@
+import { Player } from "./coomponents/Player";
+
 function App() {
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <li>
-            <span className="player-name">Player 1</span>
-            <span className="player-symbol">x</span>
-          </li>
-          <li>
-            <span className="player-name">Player 2</span>
-            <span className="player-symbol">o</span>
-          </li>
+          <Player name="Player 1" symbol="x" />
+          <Player name="Player 2" symbol="o" />
         </ol>
         GAME BOARD
       </div>
