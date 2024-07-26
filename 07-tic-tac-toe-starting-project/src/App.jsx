0f94bfefc6120660fcd80e2180b5346362f50aca
Arthuro12 <arthurfoadjo@yahoo.com>
2024-07-26 02:35:57 +0200

    Add player info section

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,5 +1,21 @@
 function App() {
-  return <p>Comming soon...</p>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">x</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">o</span>
+          </li>
+        </ol>
+        GAME BOARD
+      </div>
+    </main>
+  );
 }
 
 export default App;
