8bf77ea376e17595eeda62e4d111bae3f2a8ccdf
Arthuro12 <arthurfoadjo@yahoo.com>
2024-08-04 16:22:23 +0200

    Implement the Investment Calculator App

--- 05-starting-project/src/App.jsx
+++ 05-starting-project/src/App.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
+import Results from "./components/Results";
+import UserInput from "./components/UserInput";
 
 function App() {
-  return <Header />;
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleUserInput(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
+  const isUserInputValid = userInput.duration > 0;
+
+  return (
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChangeInput={handleUserInput} />
+      {!isUserInputValid ? (
+        <p className="center">Please enter a duration greater than zero.</p>
+      ) : null}
+      {isUserInputValid ? <Results input={userInput} /> : null}
+    </>
+  );
 }
 
 export default App;
