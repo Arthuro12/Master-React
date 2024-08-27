101da999a41c4efd8ac40f66adf5f308779e60e6
Arthuro12 <arthurfoadjo@yahoo.com>
2024-08-28 00:04:26 +0200

    Using two way binding to set the player name

--- 10-starting-project/src/components/Player.jsx
+++ 10-starting-project/src/components/Player.jsx
@@ -1,10 +1,25 @@
-export default function Player() {
+import { useRef, useState } from "react";
+
+export default function Player({}) {
+  const [playerName, setPlayerName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  function updatePlayerName(event) {
+    setSubmitted(false);
+    setPlayerName(event.target.value);
+  }
+
+  function setName() {
+    setSubmitted((oldValue) => !oldValue);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? playerName : "Unknown user"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={updatePlayerName} value={playerName} />
+        <button type="button" onClick={setName}>
+          Set Name
+        </button>
       </p>
     </section>
   );
