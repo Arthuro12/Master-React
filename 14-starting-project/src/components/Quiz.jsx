65c46c7dcb7e324b57882cb18f68cd593b70bec7
Arthuro12 <arthurfoadjo@yahoo.com>
2024-10-09 13:07:12 +0200

    Display question and register answers

--- 14-starting-project/src/components/Quiz.jsx
+++ 14-starting-project/src/components/Quiz.jsx
@@ -1,8 +1,32 @@
 import { useState } from "react";
 
+import QUESTIONS from "../questions.js";
+
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [selectedAnswers, setSelectedAnswers] = useState([]);
 
-  return <p>Currently active Question</p>;
+  const activeQuestionIndex = selectedAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setSelectedAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => {
+            return (
+              <li className="answer" key={answer}>
+                <button onClick={() => handleSelectAnswer(answer)}>
+                  {answer}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
